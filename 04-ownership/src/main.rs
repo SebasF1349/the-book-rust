39202355abf49899695df69916afdca0d2370b4d
SebasF <33849340+SebasF1349@users.noreply.github.com>
2023-05-09 23:19:14 -0300

    fixing oenwership errors

--- 04-ownership/src/main.rs
+++ 04-ownership/src/main.rs
@@ -121,4 +121,114 @@ fn main() {
             full.push_str(" Esq.");
             full
         }
+
+    //Aliasing and Murating a Data Structure
+    /* fn add_big_strings(dst: &mut Vec<String>, src: &[String]) {
+        let largest: &String = dst.iter().max_by_key(|s| s.len()).unwrap();
+        for s in src {
+            if s.len() > largest.len() {
+                dst.push(s.clone());
+            }
+        }
+    } */
+    // largest took writing permissions on dst, so it can't be mutate afterwards with dst.push(), if this was possible then push could deallocate the contents of dst, invalidating the reference largest.
+    // to fix it we need to shorten the lifetime of largest to not overlap with dst.push()
+    // possible fixes:
+        // 1. clone largest
+        fn add_big_strings2(dst: &mut Vec <String>, src: &[String]) {
+            let largest: String = dst.iter().max_by_key(|s| s.len()).unwrap().clone();
+            for s in src {
+                if s.len() > largest.len() {
+                    dst.push(s.clone());
+                }
+            }
+        }
+        // cons: performance hit for allocating and copying the string data
+        // 2. first compare and then mutate dst
+        fn add_big_strings3(dst: &mut Vec <String>, src: &[String]) {
+            let largest: &String = dst.iter().max_by_key(|s| s.len()).unwrap();
+            let to_add: Vec<String> = src.iter().filter(|s| s.len() > largest.len()).cloned().collect();
+            dst.extend(to_add);
+        }
+        // cons: performance hit for allocating the vector to_add
+        // 3. copy out the length, as we don't need the content of largest
+        fn add_big_strings4(dst: &mut Vec <String>, src: &[String]) {
+            let largest_len: usize = dst.iter().max_by_key(|s| s.len()).unwrap().len();
+            for s in src {
+                if s.len() > largest_len {
+                    dst.push(s.clone());
+                }
+            }
+        }
+        // just the most idiomatic and the most performant
+
+    // Copying vs moving out of a collection
+    // this is a safe way of copying a number out a vector
+    let v: Vec<i32> = vec![0, 1, 2];
+    let n_ref: &i32 = &v[0];
+    let n: i32 = *n_ref;
+    // but if we change the type of the elements of the vector to String, then it's not possible to dereference the value
+    /* let v: Vec<String> = vec![String::from("Hello world")];
+    let s_ref: &String = &v[0];
+    let s: String = *n_ref; */
+    // this errors because if a value does not own heap data then it can be copied without a move, so it works with i32 or with &String, but not with String
+    // posible fixes
+        // 1. avoid taking ownership of the string and use a immutable reference:
+        let v: Vec<String> = vec![String::from("Hello world")];
+        let s_ref: &String = &v[0];
+        println!("{s_ref}!");
+        // 2. clone data to get ownership while leaving the vector alone:
+        let v: Vec<String> = vec![String::from("Hello world")];
+        let mut s: String = v[0].clone();
+        s.push('!');
+        println!("{s}");
+        // 3. use Vec::remove to move the string out of the vector:
+        let mut v: Vec<String> = vec![String::from("Hello world")];
+        let mut s: String = v.remove(0);
+        s.push('!');
+        println!("{s}");
+        assert!(v.len() == 0);
+
+    // Mutating different tuple fields
+    // previous examples where of unsafe programs, but Rust can also reject safe programs. One common issue is that Rust tries to track permissions at a fine-grained level. However, Rust may conflate two different paths as the same path.
+    let mut name = (String::from("Ferris"), String::from("Rustacean"));
+    let first = &name.0;
+    name.1.push_str(", Esq.");
+    println!("{first} {},", name.1);
+    // this works because name.0 lost ownership (and so name), but not name.1
+    /* fn get_first(name: &(String, String)) -> &String {
+        &name.0
+    }
+    let mut name = (String::from("Ferris"), String::from("Rustacean"));
+    let first = get_first(&name);
+    name.1.push_str(", Esq.");
+    println!("{first} {},", name.1); */
+    // this is exactly the same as before, so it's safe, but now Rust errors, because Rust doesn't look what get_first does when deciding what get_first(&name) should borrow, so it decides that both name.0 and name.1 get borrowed.
+    // to fix it we need to use the first version or defer borrow checking to runtine with cells (which will be discussed in future chapters)
+
+    // Mutating different array elements
+    let mut a = [0, 1, 2, 3];
+    let x = &mut a[0];
+    *x += 1;
+    println!("{a:?}");
+    // Rust's borrow checker does not contain different paths for a[0], a[1], and so on, it uses a single path a[_] that represents all indexes of a. Rust does this because it cannot always determine the value of an index.
+    /* let mut a = [0, 1, 2, 3];
+    let x = &mut a[0];
+    let y = &a[1];
+    *x += *y; */
+    // this program is sage, but as a[0] was moved then a gave its read permissions to x, so a[1] can't be borrowed
+    //possible fixes:
+        // 1. use a function that can work around the borrow checker
+        let mut a = [0, 1, 2, 3];
+        let (x, rest) = a.split_first_mut().unwrap();
+        let y = &rest[0];
+        *x = *y;
+        // 2. use unsafe block
+        let mut a = [0, 1, 2, 3];
+        let x = &mut a[0] as *mut i32;
+        let y = &a[1] as *const i32;
+        unsafe { *x += *y; } // DO NOT DO THIS unless you know what you're doing!
+        // the function split_first_mut (or similar fns) implement a unsafe block inside
+
+
 }
