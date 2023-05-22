1d3b657ee69d5768852deb8382d3c76edc5e3232
SebasF <33849340+SebasF1349@users.noreply.github.com>
2023-05-21 23:18:54 -0300

    collection exercises

--- 09-collections/src/main.rs
+++ 09-collections/src/main.rs
@@ -2,6 +2,86 @@ mod vectors;
 mod strings;
 mod hash_maps;
 
+use std::{collections::HashMap, io};
+
 fn main() {
-    println!("Hello, world!");
+    // teoria en los modulos
+    // Ejercicios:
+    // 1. Given a list of integers, use a vector and return the median (when sorted, the value in the middle position) and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+    let mut nums = vec![4, 3, 2, 5, 4, 4, 2, 2, 7, 5, 4];
+    nums.sort();
+    let length = if nums.len() % 2 == 0 {nums.len() / 2} else {(nums.len() + 1) / 2};
+    let median = nums[length];
+    let mut counter = HashMap::new();
+    let mut max_quantity = 0;
+    let mut max_number = 0;
+    for num in nums {
+        let n = counter.entry(num).and_modify(|num| *num += 1).or_insert(1);
+        if n > &mut max_quantity {
+            max_quantity = *n;
+            max_number = num;
+        }
+    }
+    println!("La mediana de los número es {} y la moda es {}", median, max_number);
+
+    // 2. Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
+    let text = String::from("first");
+    let pig_latin: String;
+    let vowels = vec!['a', 'e', 'i', 'o', 'u'];
+    if !vowels.contains(&text.chars().nth(0).unwrap()) {
+        pig_latin = format!("{}-{}ay", &text[1..], text.chars().nth(0).unwrap());
+    } else {
+        pig_latin = format!("{}-hay", &text);
+    }
+    println!("{}", pig_latin);
+
+    // 3. Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
+    let mut data: HashMap<String, Vec<String>> = HashMap::new();
+    loop {
+        println!("Ingrese usuario (o escriba `n` para dejar de ingresar): ");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let input = input.trim().clone();
+                if input == "n" {
+                    break;
+                }
+                match &input.find("to") {
+                    Some(pos) => {
+                        let name = String::from(&input[4..pos-1]);
+                        let departament = String::from(&input[pos+3..]);
+                        if data.contains_key(&departament) {
+                            data.get_mut(&departament).unwrap().push(name);
+                        } else {
+                            data.insert(departament, vec![name]);
+                        }
+                    },
+                    _ => {
+                        println!("Formato incorrecto.");
+                    }
+                }
+            },
+            Err(error) => println!("Error: {}", error),
+        };
+    }
+    println!("Ingrese el nombre de un departamento para ver sus empleados o deje en blanco para verlos a todos: ");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            let input_trimmed = input.trim().clone();
+            if data.contains_key(input_trimmed) {
+                for name in data.get(input_trimmed).unwrap() {
+                    println!("{}", name);
+                }
+            } else {
+                for (k, v) in &data {
+                    println!("Department: {}", k);
+                    for name in v {
+                        println!("{}", name);
+                    }
+                }
+            }
+        },
+        Err(error) => println!("Error: {}", error),
+    }
 }
